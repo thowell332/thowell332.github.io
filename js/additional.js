cd458816dd514e9764640226d314634c60270d86
thowell332 <thowell332@gmail.com>
2020-07-02 20:33:31 -0400

    updating project portfolio

---
+++ js/additional.js
@@ -0,0 +1,15 @@
+// Modal code
+var modal = document.getElementById("paperModal");
+var btn = document.getElementById("paper");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
