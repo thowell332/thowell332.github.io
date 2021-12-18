fd803e806863f2b1c54704eaaf335edfbb2313ad
Thomas Howell <thowell332@gmail.com>
2021-12-18 12:43:30 -0500

    Move header to separate component and update README

---
+++ components/header.js
@@ -0,0 +1,21 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <link rel="stylesheet" href="assets/css/main.css" />
+            
+            <header id="header">
+                <a href="index.html" class="logo"><strong>Thomas J Howell</strong></a>
+                <ul class="icons">
+                    <li><a href="#" class="icon brands fa-github"><span class="label">Twitter</span></a></li>
+                    <li><a href="#" class="icon brands fa-linkedin"><span class="label">Facebook</span></a></li>
+                </ul>
+            </header>
+        `;
+    }
+}
+
+customElements.define('header-component', Header);
