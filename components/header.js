9e5292e27bfb0f3c1ca43764c8b075bbd6fd674c
Thomas Howell <thowell332@gmail.com>
2021-12-24 01:06:57 -0500

    Fix header links

--- components/header.js
+++ components/header.js
@@ -10,8 +10,8 @@ class Header extends HTMLElement {
             <header id="header">
                 <a href="index.html" class="logo"><strong>Thomas J Howell</strong></a>
                 <ul class="icons">
-                    <li><a href="#" class="icon brands fa-github"><span class="label">Twitter</span></a></li>
-                    <li><a href="#" class="icon brands fa-linkedin"><span class="label">Facebook</span></a></li>
+                    <li><a target="_blank" href="https://github.com/thowell332" class="icon brands fa-github"><span class="label">Twitter</span></a></li>
+                    <li><a target="_blank" href="https://www.linkedin.com/in/thomasjhowell/" class="icon brands fa-linkedin"><span class="label">Facebook</span></a></li>
                 </ul>
             </header>
         `;
